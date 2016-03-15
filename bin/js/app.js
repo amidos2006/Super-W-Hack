37c995fd7d18c03da504fdaa67b600a2781d0183
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2016-03-15 12:35:11 -0400

    "Modifing the Base Classes"

--- bin/js/app.js
+++ bin/js/app.js
@@ -7,3 +7,44 @@ var SimpleGame = (function () {
 window.onload = function () {
     var game = new SimpleGame();
 };
+var Global = (function () {
+    function Global() {
+    }
+    Global.TILE_SIZE = 32;
+    Global.ROOM_WIDTH = 11;
+    Global.ROOM_HEIGHT = 11;
+    return Global;
+})();
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var BaseGameObject = (function (_super) {
+    __extends(BaseGameObject, _super);
+    function BaseGameObject(game, x, y) {
+        _super.call(this, game);
+        this.x = x;
+        this.y = y;
+    }
+    BaseGameObject.prototype.isAnimating = function () {
+        return false;
+    };
+    return BaseGameObject;
+})(Phaser.Group);
+var BaseUIObject = (function (_super) {
+    __extends(BaseUIObject, _super);
+    function BaseUIObject(game, x, y) {
+        _super.call(this, game);
+        this.x = x;
+        this.y = y;
+    }
+    return BaseUIObject;
+})(Phaser.Group);
+var BaseGameState = (function (_super) {
+    __extends(BaseGameState, _super);
+    function BaseGameState() {
+        _super.call(this);
+    }
+    return BaseGameState;
+})(Phaser.State);
--- src/GameObjects/BaseUIObject.ts
+++ src/GameObjects/BaseUIObject.ts
@@ -1,11 +1,7 @@
-class BaseGameObject extends Phaser.Group{
+class BaseUIObject extends Phaser.Group{
     constructor(game:Phaser.Game, x:number, y:number){
         super(game);
         this.x = x;
         this.y = y;
-    }
-    
-    isAnimating(){
-        return false;
     }
 }
