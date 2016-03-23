c9ec388115a70a4be0d56c9285f12e06af564185
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2016-03-23 18:13:49 -0400

    Adding Hightlight

---
+++ src/GameObjects/FloorTiles/HighlightTile.ts
@@ -0,0 +1,22 @@
+class HighlightTile extends BaseTile{
+    sprite:Phaser.Sprite;
+    constructor(game:Phaser.Game){
+        super(game, 0, 0);
+        
+        this.sprite = this.game.add.sprite(0, 0, "graphics");
+        this.sprite.animations.add("normal", [5]);
+        this.sprite.animations.play("normal");
+        this.sprite.tint = 0x86b7c0;
+        this.add(this.sprite);
+        
+        this.alpha = 0;
+    }
+    
+    hide(){
+        this.alpha = 0;
+    }
+    
+    show(){
+        this.alpha = 1;
+    }
+}
