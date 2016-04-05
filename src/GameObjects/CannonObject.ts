2a7d0760b02b214f8395091fb05179ecafb09b89
Tiago-NYU <tiago.machado@nyu.edu>
2016-04-04 23:28:12 -0400

    class CannonObject with verification if there is a hole or a wall in the line of shot

--- src/GameObjects/CannonObject.ts
+++ src/GameObjects/CannonObject.ts
@@ -6,37 +6,89 @@ class CannonObject extends BaseGameObject
          super(game, x * Global.TILE_SIZE, y * Global.TILE_SIZE);
          this.cannonDirection = cannonDirection;
     }
+    
+    blockShoot(mapMatrix:TileTypeEnum[][])
+    {
+        var allowedToShoot = true;
+        if(this.cannonDirection.equals(new Phaser.Point(1,0)))
+        {
+            for (var index = this.getTilePosition().x+1; index < 10; index++) {
+               if(mapMatrix[index][this.getTilePosition().y] == TileTypeEnum.Hole
+                || mapMatrix[index][this.getTilePosition().y] == TileTypeEnum.Wall)
+               {
+                   allowedToShoot = false;
+               }
+            }
+        }
+        
+        else if(this.cannonDirection.equals(new Phaser.Point(-1,0)))
+        {
+            for (var index = this.getTilePosition().x-1; index > 0; index--) {
+               if(mapMatrix[index][this.getTilePosition().y] == TileTypeEnum.Hole
+                || mapMatrix[index][this.getTilePosition().y] == TileTypeEnum.Wall)
+               {
+                   allowedToShoot = false;
+               }
+            }
+        }
+        
+        else if(this.cannonDirection.equals(new Phaser.Point(0,1)))
+        {
+            for (var index = this.getTilePosition().y+1; index < 10; index++) {
+               if(mapMatrix[this.getTilePosition().x][index] == TileTypeEnum.Hole
+                || mapMatrix[this.getTilePosition().x][index] == TileTypeEnum.Wall)
+               {
+                   allowedToShoot = false;
+               }
+            }
+        }
+        
+        else if(this.cannonDirection.equals(new Phaser.Point(0,-1)))
+        {
+            for (var index = this.getTilePosition().y-1; index > 0; index--) {
+               if(mapMatrix[this.getTilePosition().x][index] == TileTypeEnum.Hole
+                || mapMatrix[this.getTilePosition().x][index] == TileTypeEnum.Wall)
+               {
+                   allowedToShoot = false;
+               }
+            }
+        }
+        
+        else
+        {
+            allowedToShoot = true;
+        }
+    }
       
-    shoot(player:PlayerObject)
+    shoot(player:PlayerObject, mapMatrix:TileTypeEnum[][])
     {
         var shot = false;
         
-        if(this.getTilePosition().y == player.getTilePosition().y
-            && this.getTilePosition().x > player.getTilePosition().x
-            && this.cannonDirection.equals(new Phaser.Point(-1,0)))
+        if (this.blockShoot(mapMatrix)) 
         {
-            shot = true;
-        }
-        
-        else if(this.getTilePosition().y == player.getTilePosition().y
-            && this.getTilePosition().x < player.getTilePosition().x
-            && this.cannonDirection.equals(new Phaser.Point(1,0)))
-        {
-            shot = true;
-        }
-        
-        else if(this.getTilePosition().x == player.getTilePosition().x
-            && this.getTilePosition().y < player.getTilePosition().y
-            && this.cannonDirection.equals(new Phaser.Point(0,1)))
-        {
-            shot = true;
-        }
-        
-        else if(this.getTilePosition().x == player.getTilePosition().x
-            && this.getTilePosition().y > player.getTilePosition().y
-            && this.cannonDirection.equals(new Phaser.Point(0,-1)))
-        {
-            shot = true;
+            if (this.getTilePosition().y == player.getTilePosition().y
+                && this.getTilePosition().x > player.getTilePosition().x
+                && this.cannonDirection.equals(new Phaser.Point(-1, 0))) {
+                shot = true;
+            }
+
+            else if (this.getTilePosition().y == player.getTilePosition().y
+                && this.getTilePosition().x < player.getTilePosition().x
+                && this.cannonDirection.equals(new Phaser.Point(1, 0))) {
+                shot = true;
+            }
+
+            else if (this.getTilePosition().x == player.getTilePosition().x
+                && this.getTilePosition().y < player.getTilePosition().y
+                && this.cannonDirection.equals(new Phaser.Point(0, 1))) {
+                shot = true;
+            }
+
+            else if (this.getTilePosition().x == player.getTilePosition().x
+                && this.getTilePosition().y > player.getTilePosition().y
+                && this.cannonDirection.equals(new Phaser.Point(0, -1))) {
+                shot = true;
+            }
         }
         return shot;
     }
