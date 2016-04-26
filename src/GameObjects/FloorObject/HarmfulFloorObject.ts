21f2080a79bbbd2e17dab7f5e16ef9226873564b
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2016-04-26 15:59:30 -0400

    lastest fix for harmfulobj

--- src/GameObjects/FloorObject/HarmfulFloorObject.ts
+++ src/GameObjects/FloorObject/HarmfulFloorObject.ts
@@ -1,9 +1,20 @@
 class HarmfulFloorObject extends BaseGameObject{
+    damage:number;
+    time:number;
+    explode:boolean;
+    
     constructor(game:Phaser.Game, xTile:number, yTile:number, damage:number, time:number, explode:boolean){
         super(game, xTile * Global.TILE_SIZE, yTile * Global.TILE_SIZE);
+        
+        this.damage = damage;
+        this.time = time;
+        this.explode = explode;
     }
     
     killObject(){
+        if(this.explode){
+            
+        }
         super.killObject();
     }
 }
