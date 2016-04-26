70b00f87c900f705b7e12c4eec3e5f04ede8c4b7
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2016-04-26 18:02:06 -0400

    New names

--- src/GameObjects/FloorObject/HarmfulFloorObject.ts
+++ src/GameObjects/FloorObject/HarmfulFloorObject.ts
@@ -15,6 +15,16 @@ class HarmfulFloorObject extends BaseGameObject{
         if(this.explode){
             
         }
+        
         super.killObject();
+    }
+    
+    updateStep(){
+        if(this.time == 0){
+            this.killObject();
+            return;
+        }
+        
+        this.time -= 1;
     }
 }
