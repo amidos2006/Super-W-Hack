4620fb92b8714277592752ac363a3025f7e565bc
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2016-04-26 15:56:49 -0400

    Temp Signature for Floor Object

--- src/GameObjects/FloorObject/HarmfulFloorObject.ts
+++ src/GameObjects/FloorObject/HarmfulFloorObject.ts
@@ -1,5 +1,9 @@
 class HarmfulFloorObject extends BaseGameObject{
-    constructor(game:Phaser.Game, xTile:number, yTile:number){
+    constructor(game:Phaser.Game, xTile:number, yTile:number, damage:number, time:number, explode:boolean){
         super(game, xTile * Global.TILE_SIZE, yTile * Global.TILE_SIZE);
+    }
+    
+    killObject(){
+        super.killObject();
     }
 }
