c578840077db92b68f5b43bba43e7261b3fc1059
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2016-04-26 14:05:14 -0400

    Now Sfxr is pressented

--- tsDefinitions/sfxr.d.ts
+++ tsDefinitions/sfxr.d.ts
@@ -1,5 +1,51 @@
 declare var sfxrMasterVolume:number;
 declare class SfxrParams{
+    //Wave Shape
+    wave_type:number;
+    
+    // Envelope
+    p_env_attack:number;
+    p_env_sustain:number;
+    p_env_punch:number;
+    p_env_decay:number;
+    
+    // Tone
+    p_base_freq:number;    // Start frequency
+    p_freq_limit:number;   // Min frequency cutoff
+    p_freq_ramp:number;    // Slide (SIGNED)
+    p_freq_dramp:number;
+    // Vibrato
+    p_vib_strength:number;
+    p_vib_speed:number;
+    
+    // Tonal change
+    p_arp_mod:number;
+    p_arp_speed:number;
+    
+    // Duty (wat's that?)
+    p_duty:number;
+    p_duty_ramp:number;
+    
+    // Repeat
+    p_repeat_speed:number;
+    
+    // Phaser
+    p_pha_offset:number;
+    p_pha_ramp:number;
+    
+    // Low-pass filter
+    p_lpf_freq:number;
+    p_lpf_ramp:number;
+    p_lpf_resonance:number;
+    // High-pass filter
+    p_hpf_freq:number;
+    p_hpf_ramp:number;
+    
+    // Sample parameters
+    sound_vol:number;
+    sample_rate:number;
+    sample_size:number;
+    
     constructor();
 }
 declare function sfxrPlaySeed(seed:number);
