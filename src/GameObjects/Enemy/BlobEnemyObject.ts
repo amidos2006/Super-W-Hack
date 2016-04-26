19a176e5c5c78552030e2986b690334deb38b8f4
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2016-04-26 18:11:18 -0400

    Fix the blob

--- src/GameObjects/Enemy/BlobEnemyObject.ts
+++ src/GameObjects/Enemy/BlobEnemyObject.ts
@@ -2,12 +2,10 @@
 
 class BlobEnemyObject extends EnemyObject implements Movement {
     firstPlayerPosition: Phaser.Point;
-    trailList: HarmfulFloorObject[];
 
     constructor(game: Phaser.Game, x: number, y: number, health: number,
         numberOfCannons: number, cannonDirection1: Phaser.Point, firstPlayerPosition: Phaser.Point) {
         super(game, x, y, health, numberOfCannons, cannonDirection1);
-        this.trailList = [];
         this.firstPlayerPosition = firstPlayerPosition;
         if (this.firstPlayerPosition.x == 10) {
             this.firstPlayerPosition.x = 1;
@@ -25,43 +23,17 @@ class BlobEnemyObject extends EnemyObject implements Movement {
     }
 
     dropTrail() {
-        var trail: HarmfulFloorObject;
-        if (!this.thereIsATrailInThisPosition(this.getTilePosition())) {
-            trail = new HarmfulFloorObject(
-                this.game,
-                this.getTilePosition().x,
-                this.getTilePosition().y,
-                1,
-                3,
-                false
-            );
-
-            this.trailList.push(trail);
-        }
-    }
-
-    thereIsATrailInThisPosition(position: Phaser.Point) {
-        if (this.trailList.length > 0) {
-            for (var index = 0; index < this.trailList.length; index++) {
-                if (this.trailList[index].position.equals(position)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        var trail: HarmfulFloorObject = new HarmfulFloorObject(this.game, 
+            this.getTilePosition().x, this.getTilePosition().y, 1, 3, false);
+        this.game.add.existing(trail);
     }
 
     enemyMove(enemyDirection: Phaser.Point, tileMatrix: TileTypeEnum[][]) {
-        var direction = new Phaser.Point
-            (
+        var direction = new Phaser.Point(
             (enemyDirection.x - this.firstPlayerPosition.x),
-            (enemyDirection.y - this.firstPlayerPosition.y)
-            );
+            (enemyDirection.y - this.firstPlayerPosition.y));
 
-        if (!this.updateEnemy(direction, tileMatrix)) {
-            this.goEnemy(new Phaser.Point(0, 0), tileMatrix);
-        }
-        else {
+        if (this.updateEnemy(direction, tileMatrix)) {
             this.goEnemy(direction, tileMatrix);
             this.dropTrail();
         }
